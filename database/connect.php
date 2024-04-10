348c54c8a30b691cd621de99fd85db95d3190e8d
Kcyberh <kcyberh30@gmail.com>
2024-04-10 22:22:06 +0100

    added a file that connect to the databse

---
+++ database/connect.php
@@ -0,0 +1,16 @@
+<?php
+$HOSTNAME ='localhost';
+$USERNAME ='root';
+$PASSWORD ='';
+$DATABASE ='db_hallmanagement';
+
+
+$conn = mysqli_connect($HOSTNAME, $USERNAME, $PASSWORD, $DATABASE);
+
+/* if ($conn){
+     echo "Database connected successfully";
+}else{
+    echo "Database connection failed";
+    //die(mysqli_error($conn));
+} */
+?>
