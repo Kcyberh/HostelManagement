1492ed76a127ec8321803f2f8e11eb9f61207ac5
Kcyberh <kcyberh30@gmail.com>
2024-04-14 22:22:58 +0100

    add rooms

---
+++ admin/adminAddRoom.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Manage Rooms</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css"> 
+    <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="main">
+        <?php 
+      include('adminMenu.php');
+      ?>
+        </div>
+      
+        
+      
+    </body>
+</html>
