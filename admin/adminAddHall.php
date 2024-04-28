660253721871b99a888fb75790bc89653861bc4f
Kcyberh <kcyberh30@gmail.com>
2024-04-28 22:04:20 +0100

    update

--- admin/adminAddHall.php
+++ admin/adminAddHall.php
@@ -17,19 +17,11 @@
     include('adminMenu.php')
     ?>
     <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-  Add Hall
-</button>
 
-<!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Hall</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
+        <h1>Add Hall</h1>
+
+      
+     
       <div class="form-floating mb-3">
   <input type="text" class="form-control" id="floatingInput" placeholder="" name="Hall_name">
   <label for="floatingInput">Name of Hall</label>
@@ -43,13 +35,12 @@
   <label for="floatingPassword">Hall Capacity</label>
 </div>
       </div>
-      <div class="modal-footer">
+      <div>
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         <button type="button" class="btn btn-primary">Save</button>
       </div>
     </div>
-  </div>
-</div>
+  
 
 <?php
 include '../database/connect.php';
