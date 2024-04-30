a5163bc751f7a5fd6d36073535fa47cb365dfe5e
Kcyberh <kcyberh30@gmail.com>
2024-04-30 23:07:36 +0100

    Fix the delete button for hall management

--- admin/deleteHall.php
+++ admin/deleteHall.php
@@ -2,13 +2,13 @@
 include '../database/connect.php';
 
 if(isset($_GET['deleteHall'])){
-    $Hall_name=$_GET['deleteHall'];
+    $hall_name=$_GET['deleteHall'];
     
     // Show a confirmation dialog before deleting
     echo "<script>
         var confirmDelete = confirm('Are you sure you want to delete this record?');
         if (confirmDelete) {
-            window.location.href = 'delete.php?confirmedDelete=true&deleteHall=".$Hall_name."';
+            window.location.href = 'deleteHall.php?confirmedDelete=true&deleteHall=".$hall_name."';
         } else {
             window.location.href = 'adminAddHall.php';
         }
@@ -17,8 +17,8 @@ if(isset($_GET['deleteHall'])){
 
 // Check if the user has confirmed the delete action
 if(isset($_GET['confirmedDelete']) && $_GET['confirmedDelete'] == "true"){
-    $Hall_name = $_GET['deleteHall'];
-    $sql = "DELETE FROM tb_hall WHERE Hall_name=$Hall_name";
+    $hall_name = $_GET['deleteHall'];
+    $sql = "DELETE FROM tb_hall WHERE Hall_name='$hall_name'";
     $result = mysqli_query($conn,$sql);
     if($result){
         // Redirect to a page indicating successful deletion
@@ -30,4 +30,4 @@ if(isset($_GET['confirmedDelete']) && $_GET['confirmedDelete'] == "true"){
         exit();
     }
 }
-?>
+?>  
